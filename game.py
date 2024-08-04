91ca50c565733750bd06bace2ef71993cf7d9741
salvakantero <salvacantero@outlook.com>
2024-08-04 18:54:28 +0200

    -

--- game.py
+++ game.py
@@ -334,7 +334,7 @@ class Game():
         
         if self.config.data['scanlines']: self.apply_scanlines()
         pygame.display.update() # refreshes the screen
-        #self.clock.tick(60) # 60 FPS
+        self.clock.tick(60) # 60 FPS
 
 
     # displays a message, darkening the screen
